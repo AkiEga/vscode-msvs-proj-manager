bc9b85490c978641b002d9ef53201552e4eb562c
akiega <akira.ega@gmail.com>
2019-10-28 23:25:15 +0900

    adapt tree nodes

--- src/msvsProj/MsvsProjNode.ts
+++ src/msvsProj/MsvsProjNode.ts
@@ -2,22 +2,56 @@ import * as vscode from 'vscode';
 import * as vsParse from "vs-parse";
 import * as path from 'path';
 
-export class MsvsProjNode extends vscode.TreeItem {
-	constructor(
-		public readonly label: string, 
-		public path: string, 
-		public readonly collapsibleState: vscode.TreeItemCollapsibleState, 
-		public readonly command?: vscode.Command) {
-		super(label, collapsibleState);
+export class MsvsProjNode extends vscode.TreeItem {	
+	public relativePath: string;
+	public rootDirPath: string;
+	public children:MsvsProjNode[];
+
+	constructor(_relativePath:string, _rootDirPath:string){
+		let label:string = _relativePath.split("\\")[0];
+		super(label, vscode.TreeItemCollapsibleState.Collapsed);
+		this.relativePath = _relativePath;
+		this.rootDirPath = _rootDirPath;
+		this.children = [];
+		console.log(this.label + " is created.\n");
 	}
 	get description(): string {
 		return this.label;
 	}
+	get fullPath():string{
+		return path.join(this.rootDirPath,this.relativePath);
+	}
 	public getBuildSettings(){
-		let p = vsParse.parseProjectSync(this.path);
+		let p = vsParse.parseProjectSync(this.fullPath);
 		console.log(p);
 	}
 	public getProjDir(){
-		return path.dirname(this.path);
+		return path.dirname(this.fullPath);
+	}
+	public addChild(childRelativePath:string, childRootDirPath:string):void{
+		let newProjNode:MsvsProjNode = new MsvsProjNode(
+			childRelativePath,
+			childRootDirPath			
+		);
+		if(newProjNode.label === ""){
+			return;
+		}
+		for(let c of this.children){
+			if(c.label === newProjNode.label){
+				return;
+			}
+		}
+
+		console.log(newProjNode.label + " is created.\n");
+		this.children.push(newProjNode);
+		
+		let childNodePath:string = '';
+		let dirs:string[] = childRelativePath.split('\\');
+		for(let i=1;i<dirs.length;i++){
+			if(dirs[i]){
+				childNodePath += '/' + dirs[i];
+			}
+		}
+		newProjNode.addChild(childNodePath, childRootDirPath);
 	}
 }
--- src/msvsProj/MsvsProjProvider.ts
+++ src/msvsProj/MsvsProjProvider.ts
@@ -7,23 +7,16 @@ export default class MsvsProjProvider implements vscode.TreeDataProvider<MsvsPro
 	private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
 	readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
 	private rootDirPath:string;
-	private msvsProjNodes:MsvsProjNode[];
+	private msvsProjRootNode:MsvsProjNode;
 
 	constructor(private readonly tarSlnFilePath:string) { 
 		this.rootDirPath = path.dirname(tarSlnFilePath);
 		let solutionData:any = vsParse.parseSolutionSync(tarSlnFilePath);
-		this.msvsProjNodes = [];
+		this.msvsProjRootNode = new MsvsProjNode(".", this.rootDirPath);
 		for(let p of solutionData.projects){
-			let msvsProjFullPath:string = path.join(this.rootDirPath, p.relativePath);
-			let newProjNode:MsvsProjNode = new MsvsProjNode(
-				p.relativePath,
-				msvsProjFullPath,
-				vscode.TreeItemCollapsibleState.Collapsed
-			);
-			this.msvsProjNodes.push(newProjNode);
-		}
+			this.msvsProjRootNode.addChild(p.relativePath,this.rootDirPath);
+		}	
 	}
-
 	public refresh(): any {
 		this._onDidChangeTreeData.fire();
 	}
@@ -34,13 +27,14 @@ export default class MsvsProjProvider implements vscode.TreeDataProvider<MsvsPro
 
 	public getChildren(element?: MsvsProjNode): MsvsProjNode[] | Thenable<MsvsProjNode[]> {
 		let ret:MsvsProjNode[] = [];
-		//let currentPathPos = path.join(this.rootDirPath, element.path);
 		if(element){
-			if(path.dirname(element.path) === this.rootDirPath){
-				ret = this.msvsProjNodes;
+			if(path.dirname(element.fullPath) === this.rootDirPath){
+				ret = this.msvsProjRootNode.children;
+			}else{
+				ret = element.children;
 			}
 		}else{
-			ret = this.msvsProjNodes;
+			ret = this.msvsProjRootNode.children;
 		}
 		return ret;
 	}
