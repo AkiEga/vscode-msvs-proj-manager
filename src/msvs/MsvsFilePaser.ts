852a2d3f76fd4afc832486aaf57c6de0c912ca0d
akiega <akira.ega@gmail.com>
2019-11-03 09:41:03 +0900

    temp update(not done)

--- src/msvs/MsvsFilePaser.ts
+++ src/msvs/MsvsFilePaser.ts
@@ -1,5 +1,10 @@
+//import { MsvsProjFile } from './MsvsFilePaser';
 import * as path from 'path';
 import * as vscode from 'vscode';
+import * as vsParse from "vs-parse";
+import * as fs from 'fs';
+import MsvsProjProvider from './MsvsProjProvider';
+import { match } from 'minimatch';
 
 class MyPath{
 	public cwd:string; 		// full path of current work directory 
@@ -13,18 +18,17 @@ class MyPath{
 	}
 }
 
-class MsvsProjFileTree{
-	public children:MsvsProjFile[];
-}
-
-export class MsvsProjFile extends vscode.TreeItem {
+export class SlnFileManager extends vscode.TreeItem {
 	public id: string;	
 	public label: string;
 	public realFilePath: MyPath;
 	public idealFilePath: MyPath;
 	public relativePath: string;
 	public rootDirPath: string;
-	public children:MsvsProjFile[];
+	public children:SlnFileManager[];
+
+	public projects:MsvsProj[];
+	public projectsTree:MsvsProjTree;
 
 	constructor(_relativePath:string, _rootDirPath:string){
 		let label:string = _relativePath.split("\\").pop();
@@ -32,7 +36,113 @@ export class MsvsProjFile extends vscode.TreeItem {
 		this.relativePath = _relativePath;
 		this.rootDirPath = _rootDirPath;
 		this.children = [];
+
+		this.projects = [];		
+		this.projectsTree = new MsvsProjTree();
 	}
+	public ReadSlnFile(tarSlnFilePath:string){
+		// read file
+		fs.readFile(tarSlnFilePath, 'utf-8', (err, data)=>{
+			let lines:string[] = data.split('\n');			
+			this.Parse(lines,0);
+		});
+	}
+
+	private Parse(lines:string[],lineIndex:number):void{	
+		if(lines.length > lineIndex){
+			let l = lines[lineIndex];
+			// Project("{<Parent GUID>}") = "<Element Label>", "<Element Real File Path>", "{<Elemnt GUID>}"
+			// EndProject
+			if(l.match(/^\s*Project.*$/g)){
+				lineIndex = this.ParsedForProj(lines,lineIndex);
+			}else if(l.match(/^\s*GlobalSection\(NestedProjects\).*$/g)){
+				lineIndex = this.ParsedForGlobalSection(lines,lineIndex);
+			}
+
+		}
+		return;
+	}
+
+	private ParsedForProj(lines:string[],lineIndex:number):number{
+		let newLineIndex:number = lineIndex;
+		if(lines.length > lineIndex){
+			let line = lines[lineIndex];
+
+			let parsedLine:string[] = [];
+			let regExp:RegExp = new RegExp('(Project)|[\(\)\{\}]','g');
+			let filtered:string = line.replace(regExp,"");
+			let splited:string[] = filtered.split(/[,=]/g);
+			for(let s of splited){
+				parsedLine.push(s.trim().replace(/"/g,''));
+			}
+			this.projects.push(new MsvsProj(parsedLine[0],parsedLine[1],parsedLine[2],parsedLine[3]));
+
+			// search "EndProject"
+			for(let i=lineIndex+1;i<lines.length;i++){
+				line = lines[i];
+				if(line.match(/^\s*EndProject$/)){
+					newLineIndex = i;
+					break;
+				}
+			}
+		}else{
+			newLineIndex = -1;
+		}
+		return newLineIndex;
+	}
+	private ParsedForGlobal(lines:string[],lineIndex:number):number{
+		let newLineIndex:number = lineIndex;
+		if(lines.length > lineIndex){
+			let line = lines[lineIndex];
+
+			let parsedLine:string[] = [];
+			let regExp:RegExp = new RegExp('(Global)|[\(\)\{\}]','g');
+			let filtered:string = line.replace(regExp,"");
+			let splited:string[] = filtered.split(/[,=]/g);
+			for(let s of splited){
+				parsedLine.push(s.trim().replace(/"/g,''));
+			}
+			this.projects.push(new MsvsProj(parsedLine[0],parsedLine[1],parsedLine[2],parsedLine[3]));
+
+			// search "EndProject"
+			for(let i=lineIndex+1;i<lines.length;i++){
+				line = lines[i];
+				if(line.match(/^EndGlobal$/)){
+					newLineIndex = i;
+					break;
+				}
+			}
+		}else{
+			newLineIndex = -1;
+		}
+		return newLineIndex;
+	}
+	private ParsedForGlobalSection(lines:string[],lineIndex:number):number{
+		let newLineIndex:number = lineIndex;
+		if(lines.length > lineIndex){
+			let line = lines[lineIndex];
+
+			for(let i=lineIndex+1;i<lines.length;i++){
+				line = lines[i];
+
+				let matched:string[] = line.match(/^\s*{([^{}]*)} = {([^{}]*)}\s*$/g);
+				let parentProjGUID = matched[0];
+				let childProjGUID = matched[1];
+				this.projects.find((value,index,obj)=>{
+
+				});
+
+				if(line.match(/^\s*EndGlobalSection$/)){
+					newLineIndex = i;
+					break;
+				}
+			}
+		}else{
+			newLineIndex = -1;
+		}
+		return newLineIndex;
+	}
+
 	get description(): string {
 		return this.label;
 	}
@@ -42,9 +152,10 @@ export class MsvsProjFile extends vscode.TreeItem {
 	public getProjDir(){
 		return path.dirname(this.fullPath);
 	}
+
 	public addChild(childRelativePath:string,fullDepthPath:string):void{
 		// add current children nodes		
-		let newProjNode:MsvsProjFile = this.CreateNodeAndAddChild(childRelativePath);
+		let newProjNode:SlnFileManager = this.CreateNodeAndAddChild(childRelativePath);
 
 		// create next children nodes
 		if(newProjNode){			
@@ -54,11 +165,11 @@ export class MsvsProjFile extends vscode.TreeItem {
 		}
 	}
 
-	private CreateNodeAndAddChild(childRelativePath:string):MsvsProjFile{
+	private CreateNodeAndAddChild(childRelativePath:string):SlnFileManager{
 		if(childRelativePath=== undefined){
 			return null;
 		}
-		let newProjNode:MsvsProjFile = new MsvsProjFile(
+		let newProjNode:SlnFileManager = new SlnFileManager(
 			childRelativePath,
 			this.rootDirPath			
 		);
@@ -92,24 +203,49 @@ export class MsvsProjFile extends vscode.TreeItem {
 			nextChildNodePath = path.join(childRelativePath,nextDirname);	
 		}
 		return nextChildNodePath;
-	}}
-
-export class MsvsSlnFile {
+	}
+}
+class MsvsProjTree{
+	public own:MsvsProj;
+	public children:MsvsProj[];
+	public addChild(newProj:MsvsProj){
+		let doAdd:boolean = true;
+		// check dupulication
+		for(let c of this.children){
+			if(c.OwnGUID === newProj.OwnGUID){
+				doAdd = false;
+			}
+		}
+		// do addition
+		if(doAdd === true){
+			this.children.push(newProj);
+		}
+	}
+}
+class MsvsSlnFile {
 	public readonly id:string;
 	public realFilePath:MyPath;
-	public rootMsvsProjFile:MsvsProjFile;
+	public rootMsvsProjFile:SlnFileManager;
 	constructor(_cwd:string, _realFullFilePath:string){
 		this.realFilePath = new MyPath(_cwd, _realFullFilePath);
+	}
+}
+class MsvsProj{
+	constructor(
+		public ParentGUID:string,
+		public label:string,
+		public OwnGUID:string,
+		public FilePath:string
+		){			
 	}
 }
 
 export default class MsvsFilePaser{
 	public sln:MsvsSlnFile;
-	constructor(){
-
-	}
-	public ParseSlnFile(){
-
+	public tree:MsvsProjTree;
+	public ParseSlnFile(tarSlnFilePath:string){
+		this.sln = vsParse.parseSolutionSync(tarSlnFilePath);
+		this.tree = new MsvsProjTree;
 	}
 
 }
--- src/msvs/MsvsProjProvider.ts
+++ src/msvs/MsvsProjProvider.ts
@@ -1,13 +1,13 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as vsParse from "vs-parse";
-import { MsvsProjFile} from './MsvsFilePaser';
+import { SlnFileManager} from './MsvsFilePaser';
 
-export default class MsvsProjProvider implements vscode.TreeDataProvider<MsvsProjFile> {
+export default class MsvsProjProvider implements vscode.TreeDataProvider<SlnFileManager> {
 	private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
 	readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
 	private rootDirPath:string;
-	private msvsProjRootNode:MsvsProjFile;
+	private msvsProjRootNode:SlnFileManager;
 	private tarSlnFilePath:string;
 
 	constructor(private readonly _tarSlnFilePath:string) { 
@@ -17,11 +17,14 @@ export default class MsvsProjProvider implements vscode.TreeDataProvider<MsvsPro
 	private init():void{
 		this.rootDirPath = path.dirname(this.tarSlnFilePath);
 		let solutionData:any = vsParse.parseSolutionSync(this.tarSlnFilePath);
-		this.msvsProjRootNode = new MsvsProjFile(".", this.rootDirPath);
+		this.msvsProjRootNode = new SlnFileManager(".", this.rootDirPath);
 		for(let p of solutionData.projects){
 			let topDir:string = p.relativePath.split("\\")[0];
 			this.msvsProjRootNode.addChild(topDir, p.relativePath);
 		}	
+
+		let mpf = new SlnFileManager(this.tarSlnFilePath, this.rootDirPath);
+		mpf.ReadSlnFile(this.tarSlnFilePath);
 		return;
 	}
 
@@ -29,12 +32,12 @@ export default class MsvsProjProvider implements vscode.TreeDataProvider<MsvsPro
 		this._onDidChangeTreeData.fire();
 	}
 
-	public getTreeItem(element: MsvsProjFile): vscode.TreeItem {
+	public getTreeItem(element: SlnFileManager): vscode.TreeItem {
 		return element;
 	}
 
-	public getChildren(element?: MsvsProjFile): MsvsProjFile[] | Thenable<MsvsProjFile[]> {
-		let ret:MsvsProjFile[] = [];
+	public getChildren(element?: SlnFileManager): SlnFileManager[] | Thenable<SlnFileManager[]> {
+		let ret:SlnFileManager[] = [];
 		if(element){
 			if(path.dirname(element.fullPath) === this.rootDirPath){
 				ret = this.msvsProjRootNode.children;
